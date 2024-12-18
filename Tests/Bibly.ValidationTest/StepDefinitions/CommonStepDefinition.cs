34fff4517b6495fad4d1fb34a3fe10ca09204b5c
Jérémy RICHARD <jeremy.richard@vinci-construction.com>
2024-12-18 10:38:04 +0100

    add common step def

---
+++ Tests/Bibly.ValidationTest/StepDefinitions/CommonStepDefinition.cs
@@ -0,0 +1,16 @@
+﻿using Bibly.Application.Common.Exceptions;
+
+namespace Bibly.ValidationTest.StepDefinitions
+{
+    public class CommonStepDefinition : Testing
+    {
+        public Exception? exception;
+
+        [Then("une erreur de validation est retournee")]
+        public void ThenUneErreurDeValidationEstRetournee()
+        {
+            exception.Should().NotBeNull();
+            exception.Should().BeOfType<ValidationException>();
+        }
+    }
+}
--- Tests/Bibly.ValidationTest/StepDefinitions/GestionDesAuteursStepDefinitions.cs
+++ Tests/Bibly.ValidationTest/StepDefinitions/GestionDesAuteursStepDefinitions.cs
@@ -1,18 +1,15 @@
 using Bibly.Application.Author.Command;
-using Bibly.Application.Common.Exceptions;
 using Bibly.ValidationTest.Drivers;
 
 namespace Bibly.ValidationTest.StepDefinitions
 {
     [Binding]
-    public class GestionDesAuteursStepDefinitions : Testing, IDisposable
+    public class GestionDesAuteursStepDefinitions : CommonStepDefinition, IDisposable
     {
         private AddAuthorCommand command;
         private string LastName;
         private string FirstName;
         private DateTime BirthDay;
-
-        private Exception exception;
 
         public GestionDesAuteursStepDefinitions()
         {
@@ -73,13 +70,6 @@ namespace Bibly.ValidationTest.StepDefinitions
                 exception = ex;
             }
 
-        }
-
-        [Then("une erreur de validation est retournee")]
-        public void ThenUneErreurDeValidationEstRetournee()
-        {
-            exception.Should().NotBeNull();
-            exception.Should().BeOfType<ValidationException>();
         }
 
         public void Dispose() => FakeAuthorRepository.Authors.Clear();
